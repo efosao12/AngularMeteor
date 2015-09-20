9600926370349b31a215d1fc59c24d7e63a41d61
Efosa Osamwonyi <efosa.osamwonyi346@gmail.com>
2015-09-20 03:25:15 -0400

    progress 1

---
+++ app.js
@@ -0,0 +1,28 @@
+Parties = new Mongo.Collection("parties");
+
+if (Meteor.isClient){
+   angular.module('socially',['angular-meteor']);  
+    
+    angular.module('socially').controller('PartiesListCtrl',['$scope','$meteor',function($scope,$meteor){
+        $scope.parties = $meteor.collection(Parties);
+    }]);
+    
+    
+} 
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    if (Parties.find().count() === 0) {
+      var parties = [
+        {'name': 'Dubstep-Free Zone',
+          'description': 'Fast just got faster with Nexus S.'},
+        {'name': 'All dubstep all the time',
+          'description': 'Get it on!'},
+        {'name': 'Savage lounging',
+          'description': 'Leisure suit required. And only fiercest manners.'}
+      ];
+      for (var i = 0; i < parties.length; i++)
+        Parties.insert(parties[i]);
+    }
+  });
+}
