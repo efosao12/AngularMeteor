3afc3ad39c61d2f5bfdb0fbf3b387e501a936f3c
Efosa Osamwonyi <efosa.osamwonyi346@gmail.com>
2015-09-20 03:32:27 -0400

    mongo add/delete

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ if (Meteor.isClient){
    angular.module('socially',['angular-meteor']);  
     
     angular.module('socially').controller('PartiesListCtrl',['$scope','$meteor',function($scope,$meteor){
-        $scope.parties = $meteor.collection(Parties);
+        $scope.parties = $meteor.collection(Parties);  
+        
+$scope.remove= function(party){       
+    $scope.parties.splice($scope.parties.indexOf(party), 1);
+      };
     }]);
     
     
@@ -23,6 +27,6 @@ if (Meteor.isServer) {
       ];
       for (var i = 0; i < parties.length; i++)
         Parties.insert(parties[i]);
-    }
+    };
   });
 }
