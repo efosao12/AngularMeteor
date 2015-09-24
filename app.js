76d44a82475a35069f7a09d0c48ae9a835b8aae3
Efosa Osamwonyi <efosa.osamwonyi346@gmail.com>
2015-09-23 22:53:29 -0400

    binding

--- app.js
+++ app.js
@@ -1,61 +1,67 @@
 Parties = new Mongo.Collection("parties");
 
 if (Meteor.isClient) {
-  angular.module('socially', ['angular-meteor', 'ui.router']);
+    angular.module('socially', ['angular-meteor', 'ui.router']);
 
-  angular.module('socially').config(['$urlRouterProvider', '$stateProvider', '$locationProvider',
-    function($urlRouterProvider, $stateProvider, $locationProvider){
+    angular.module('socially').config(['$urlRouterProvider', '$stateProvider', '$locationProvider',
+    function ($urlRouterProvider, $stateProvider, $locationProvider) {
 
-      $locationProvider.html5Mode(true);
+            $locationProvider.html5Mode(true);
 
-      $stateProvider
-        .state('parties', {
-          url: '/parties',
-          templateUrl: 'parties-list.ng.html',
-          controller: 'PartiesListCtrl'
-        })
-        .state('partyDetails', {
-          url: '/parties/:partyId',
-          templateUrl: 'party-details.ng.html',
-          controller: 'PartyDetailsCtrl'
-        });
+            $stateProvider
+                .state('parties', {
+                    url: '/parties',
+                    templateUrl: 'parties-list.ng.html',
+                    controller: 'PartiesListCtrl'
+                })
+                .state('partyDetails', {
+                    url: '/parties/:partyId',
+                    templateUrl: 'party-details.ng.html',
+                    controller: 'PartyDetailsCtrl'
+                });
 
-      $urlRouterProvider.otherwise("/parties");
+            $urlRouterProvider.otherwise("/parties");
     }]);
 
-  angular.module('socially').controller('PartiesListCtrl', ['$scope', '$meteor', function ($scope, $meteor) {
-    $scope.parties = $meteor.collection(Parties);
+    angular.module('socially').controller('PartiesListCtrl', ['$scope', '$meteor', function ($scope, $meteor) {
+        $scope.parties = $meteor.collection(Parties);
 
-    $scope.remove = function(party){
-      $scope.parties.remove(party);
-    };
+        $scope.remove = function (party) {
+            $scope.parties.remove(party);
+        };
 
-    $scope.removeAll = function(){
-      $scope.parties.remove();
-    };
+        $scope.removeAll = function () {
+            $scope.parties.remove();
+        };
   }]);
 
-  angular.module("socially").controller("PartyDetailsCtrl", ['$scope', '$stateParams',
-    function($scope, $stateParams){
+    angular.module("socially").controller("PartyDetailsCtrl", ['$scope', '$stateParams', '$meteor',
+    function ($scope, $stateParams,$meteor) {
 
-      $scope.partyId = $stateParams.partyId;
+            $scope.party = $meteor.object(Parties,$stateParams.partyId);
 
     }]);
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    if (Parties.find().count() === 0) {
-      var parties = [
-        {'name': 'Dubstep-Free Zone',
-          'description': 'Fast just got faster with Nexus S.'},
-        {'name': 'All dubstep all the time',
-          'description': 'Get it on!'},
-        {'name': 'Savage lounging',
-          'description': 'Leisure suit required. And only fiercest manners.'}
+    Meteor.startup(function () {
+        if (Parties.find().count() === 0) {
+            var parties = [
+                {
+                    'name': 'Dubstep-Free Zone',
+                    'description': 'Fast just got faster with Nexus S.'
+                },
+                {
+                    'name': 'All dubstep all the time',
+                    'description': 'Get it on!'
+                },
+                {
+                    'name': 'Savage lounging',
+                    'description': 'Leisure suit required. And only fiercest manners.'
+                }
       ];
-      for (var i = 0; i < parties.length; i++)
-        Parties.insert(parties[i]);
-    }
-  });
+            for (var i = 0; i < parties.length; i++)
+                Parties.insert(parties[i]);
+        }
+    });
 }
