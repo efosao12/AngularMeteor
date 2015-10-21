45697f8f4aba3ea19be0c0b26a24534ff28094bd
Efosa Osamwonyi <efosa.osamwonyi346@gmail.com>
2015-10-21 18:51:02 -0400

    Auth on routes user

--- client/routes.js
+++ client/routes.js
@@ -1,3 +1,15 @@
+angular.module("socially").run(function ($rootScope, $state) {
+    $rootScope.$on('$stateChangeError', function (event, toState, toParams, fromState, fromParams, error) {
+        // We can catch the error thrown when the $requireUser promise is rejected
+        // and redirect the user back to the main page
+        if (error === 'AUTH_REQUIRED') {
+            $state.go('parties');
+        }
+    });
+});
+
+
+
 angular.module('socially').config(['$urlRouterProvider', '$stateProvider', '$locationProvider',
     function ($urlRouterProvider, $stateProvider, $locationProvider) {
 
@@ -13,6 +25,11 @@ angular.module('socially').config(['$urlRouterProvider', '$stateProvider', '$loc
                 url: '/parties/:partyId',
                 templateUrl: 'client/parties/views/party-details.ng.html',
                 controller: 'PartyDetailsCtrl'
+                resolve: {
+                    "currentUser": function ($meteor) {
+                        return $meteor.requireUser();
+                    }
+                }
             });
         $urlRouterProvider.otherwise("/parties");
     }]);
