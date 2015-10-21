847a8d8dc03844e99e7db5e59ebf467c8ff61e0e
Efosa Osamwonyi <efosa.osamwonyi346@gmail.com>
2015-10-21 19:13:41 -0400

    public and not public

--- client/routes.js
+++ client/routes.js
@@ -8,28 +8,25 @@ angular.module("socially").run(function ($rootScope, $state) {
     });
 });
 
+angular.module("socially").config(function ($urlRouterProvider, $stateProvider, $locationProvider) {
+    $locationProvider.html5Mode(true);
 
-
-angular.module('socially').config(['$urlRouterProvider', '$stateProvider', '$locationProvider',
-    function ($urlRouterProvider, $stateProvider, $locationProvider) {
-
-        $locationProvider.html5Mode(true);
-
-        $stateProvider
-            .state('parties', {
-                url: '/parties',
-                templateUrl: 'client/parties/views/parties-list.ng.html',
-                controller: 'PartiesListCtrl'
-            })
-            .state('partyDetails', {
-                url: '/parties/:partyId',
-                templateUrl: 'client/parties/views/party-details.ng.html',
-                controller: 'PartyDetailsCtrl'
-                resolve: {
-                    "currentUser": function ($meteor) {
-                        return $meteor.requireUser();
-                    }
+    $stateProvider
+        .state('parties', {
+            url: '/parties',
+            templateUrl: 'client/parties/views/parties-list.ng.html',
+            controller: 'PartiesListCtrl'
+        })
+        .state('partyDetails', {
+            url: '/parties/:partyId',
+            templateUrl: 'client/parties/views/party-details.ng.html',
+            controller: 'PartyDetailsCtrl',
+            resolve: {
+                "currentUser": function ($meteor) {
+                    return $meteor.requireUser();
                 }
-            });
-        $urlRouterProvider.otherwise("/parties");
-    }]);
+            }
+        });
+
+    $urlRouterProvider.otherwise("/parties");
+});
